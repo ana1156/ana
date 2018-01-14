7382f3ef1ba36d4cab1e3253342f527679956a38
ana1156 <35394420+ana1156@users.noreply.github.com>
2018-01-15 00:01:20 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Hello
+{
+public static void main(String ag[])
+{
+int n,i;
+Scanner x=new Scanner(System.in);
+System.out.println("enter n");
+n=x.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.println("Hello");}}}
