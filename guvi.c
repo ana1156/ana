dbc52b50ba443e6e7c325958564fc46517403998
ana1156 <35394420+ana1156@users.noreply.github.com>
2018-01-14 21:20:59 +0530

    Add files via upload

--- guvi.c
+++ guvi.c
@@ -4,7 +4,9 @@ void main()
 	int n;
 	printf("enter a number between 1 and 100000 both inclusive");
 	scanf("%d",&n);
-	if (n%2==0)
-	printf("even");
-	else printf("odd");
+	if (n<0)
+	printf("Positive");
+	else if (n>0)
+	printf("Negative");
+	else printf("Zero");
 }
